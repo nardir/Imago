c354e8e3f6ce9938236063e798ea2e01a699f353
Nardi <nardir@axerrio.com>
2015-04-21 13:54:59 +0200

    Image Resizing

--- Axerrio.Images.Test/Program.cs
+++ Axerrio.Images.Test/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Axerrio.Images.AOL;
+using System.Drawing.Imaging;
 
 namespace Axerrio.Images.Test
 {
@@ -15,8 +19,19 @@ namespace Axerrio.Images.Test
 
         public static void TestImage()
         {
-            ImageManager im = new ImageManager();
-            im.Resize();
+            var filePath = @"C:\Users\Nardi\Documents\Visual Studio 2013\Projects\Imago\Images\649_3.jpg";
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                using (Image image = Image.FromStream(fileStream))
+                {
+                    var image200200 = image.Resize(200,200);
+
+                    using (var resizedFile = new FileStream(@"C:\Users\Nardi\Documents\Visual Studio 2013\Projects\Imago\ResizedImages\649_3_200x200.jpg", FileMode.CreateNew))
+                    {
+                        image200200.Save(resizedFile, ImageFormat.Jpeg);
+                    }
+                }
+            }
         }
     }
 }

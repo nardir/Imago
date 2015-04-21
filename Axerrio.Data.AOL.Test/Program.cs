2863ec02b0b30af56e581d1e34259653dfa039ea
Nardi <nardir@axerrio.com>
2015-04-21 12:09:01 +0200

    DAL Test getArticles

---
+++ Axerrio.Data.AOL.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Axerrio.Data.AOL;
+using Axerrio.Data.AOL.Model;
+using Axerrio.Data.AOL.Repository;
+
+namespace Axerrio.Data.AOL.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestGetArticles();
+        }
+
+        static void TestGetArticles()
+        {
+            try
+            {
+                using (IArticleRepository repo = new ArticleRepository())
+                {
+                    var articles = repo.GetArticlesAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ Axerrio.Data.AOL/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Axerrio.Data.AOL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axerrio.Data.AOL
+{
+    public interface IArticleRepository : IDisposable
+    {
+        Task<IEnumerable<Article>> GetArticlesAsync(); 
+    }
+}
